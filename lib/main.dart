3b129129aed69eb6448c136f03431be21d1c1b14
Hiram Castillo Uni <hiramcastillouni@MacBook-Air-de-Hiram.local>
2025-10-05 04:56:15 -0600

    actividades

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SKAI',
       theme: buildTheme(context),
-        home: ActivitiesPage(),
+        home: const NavigationShell(),
         routes: {
           '/login': (context) => const AuthPage(),
         },
