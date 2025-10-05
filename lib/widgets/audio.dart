d84a4ecce6064220257ead771a9c329391496f79
Hiram Castillo Uni <hiramcastillouni@MacBook-Air-de-Hiram.local>
2025-10-04 20:10:54 -0600

    feat: text to speech

--- lib/widgets/audio.dart
+++ lib/widgets/audio.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:flutter_tts/flutter_tts.dart';
+
 
 class SpeechToTextExample extends StatefulWidget {
   @override
